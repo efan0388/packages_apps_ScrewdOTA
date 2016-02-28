SlimSaber/packages_apps_SlimOTA

Show current changelog
Changelog file should be placed in /system/etc/changelog.txt

app/src/main/java/com/fusionjack/slimota/MainActivity.java
@@ -46,6 +46,11 @@ public class MainActivity extends PreferenceActivity implements
     }
 
     @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return SlimOTAFragment.class.getName().equalsIgnoreCase(fragmentName);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button

app/src/main/java/com/fusionjack/slimota/fragments/ChangelogFragment.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2015 Chandra Poerwanto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fusionjack.slimota.fragments;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fusionjack.slimota.R;
+import com.fusionjack.slimota.utils.Changelog;
+
+public class ChangelogFragment extends PreferenceFragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.build_changelog, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Changelog rc = new Changelog() {
+            @Override
+            public void onResponseReceived(String result) {
+                final TextView changelogTextView = (TextView) getView().findViewById(R.id.tv_changelog);
+                if (changelogTextView == null) {
+                    return;
+                }
+                changelogTextView.setText(result);
+            }
+        };
+        rc.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, getActivity());
+    }
+}

app/src/main/java/com/fusionjack/slimota/utils/Changelog.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2015 Chandra Poerwanto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fusionjack.slimota.utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.fusionjack.slimota.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+interface IChangelog {
+    public void onResponseReceived(String result);
+}
+
+public abstract class Changelog extends AsyncTask<Context, Integer, String> implements IChangelog {
+
+    private static final String CHANGELOG_FILE = "/system/etc/changelog.txt";
+
+    public abstract void onResponseReceived(String result);
+
+    @Override
+    protected String doInBackground(Context... arg) {
+        Context context = arg[0];
+        File file = new File(CHANGELOG_FILE);
+        if (!file.exists()) {
+            return context.getString(R.string.no_changelog_summary);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Scanner scanner = null;
+        String changelog;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                sb.append(line);
+                sb.append("\n");
+            }
+            changelog = sb.toString();
+        } catch (FileNotFoundException ex) {
+            changelog = ex.getMessage();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        return changelog;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        onResponseReceived(result);
+    }
+}

efan0388/packages_apps_ScrewdOTA

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

app/src/main/java/com/fusionjack/screwdota/MainActivity.java
@@ -46,6 +46,11 @@ public class MainActivity extends PreferenceActivity implements
     }
 
     @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return ScrewdOTAFragment.class.getName().equalsIgnoreCase(fragmentName);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button

app/src/main/java/com/fusionjack/screwdota/fragments/ChangelogFragment.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2015 Chandra Poerwanto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fusionjack.screwdota.fragments;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fusionjack.screwdota.R;
+import com.fusionjack.screwdota.utils.Changelog;
+
+public class ChangelogFragment extends PreferenceFragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.build_changelog, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Changelog rc = new Changelog() {
+            @Override
+            public void onResponseReceived(String result) {
+                final TextView changelogTextView = (TextView) getView().findViewById(R.id.tv_changelog);
+                if (changelogTextView == null) {
+                    return;
+                }
+                changelogTextView.setText(result);
+            }
+        };
+        rc.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, getActivity());
+    }
+}

app/src/main/java/com/fusionjack/screwdota/utils/Changelog.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2015 Chandra Poerwanto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fusionjack.screwdota.utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.fusionjack.screwdota.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+interface IChangelog {
+    public void onResponseReceived(String result);
+}
+
+public abstract class Changelog extends AsyncTask<Context, Integer, String> implements IChangelog {
+
+    private static final String CHANGELOG_FILE = "/system/etc/changelog.txt";
+
+    public abstract void onResponseReceived(String result);
+
+    @Override
+    protected String doInBackground(Context... arg) {
+        Context context = arg[0];
+        File file = new File(CHANGELOG_FILE);
+        if (!file.exists()) {
+            return context.getString(R.string.no_changelog_summary);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Scanner scanner = null;
+        String changelog;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                sb.append(line);
+                sb.append("\n");
+            }
+            changelog = sb.toString();
+        } catch (FileNotFoundException ex) {
+            changelog = ex.getMessage();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        return changelog;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        onResponseReceived(result);
+    }
+}